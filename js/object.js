70f3e78d0ededc994d8825689178bbae67a68109
Anthony Burns <aburns42@gmail.com>
2022-04-30 20:13:04 -0400

    Created js files for array of objects and to print array to table.

---
+++ js/object.js
@@ -0,0 +1,82 @@
+var array = [
+    {
+        num: 1,
+        name: "Jean-Luc Picard",
+        office: "Command",
+        product: "certificate",
+        title: "30-year length of service",
+        completedDate: "01/05/2022"
+    },
+    {
+        num: 2,
+        name: "Will Riker",
+        office: "Command",
+        product: "poster",
+        title: "Promotion poster",
+        completedDate: "01/20/2022"
+    },
+    {
+        num: 3,
+        name: "Geordi La Forge",
+        office: "Engineering",
+        product: "newsletter",
+        title: "Warp drive do's and don'ts",
+        completedDate: "02/10/2022"
+    },
+    {
+        num: 4,
+        name: "Worf",
+        office: "Security",
+        product: "document",
+        title: "Security Monthly layout",
+        completedDate: "02/25/2022"
+    },
+    {
+        num: 5,
+        name: "Beverly Crusher",
+        office: "Medical",
+        product: "certificate",
+        title: "Outstanding Service award",
+        completedDate: "03/01/2022"
+    },
+    {
+        num: 6,
+        name: "Deanna Troi",
+        office: "Counsel",
+        product: "certificate",
+        title: "Special Service award",
+        completedDate: "03/14/2022"
+    },
+    {
+        num: 7,
+        name: "Data",
+        office: "Operations",
+        product: "document",
+        title: "Android Maintenance layout",
+        completedDate: "04/22/2022"
+    },
+    {
+        num: 8,
+        name: "Wesley Crusher",
+        office: "Operations",
+        product: "graphic",
+        title: "Earth Day graphic",
+        completedDate: "04/30/2022"
+    },
+    {
+        num: 9,
+        name: "Guinan",
+        office: "Ten Forward",
+        product: "document",
+        title: "Ten Forward Menu layout",
+        completedDate: "05/10/2022"
+    },
+    {
+        num: 10,
+        name: "Miles O'Brien",
+        office: "Engineering",
+        product: "graphic",
+        title: "Engineering Story graphic",
+        completedDate: "05/23/2022"
+    }
+];
