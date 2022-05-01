c3c0b0d620d2a8aa2001641bdeb26f2192f9165b
Anthony Burns <aburns42@gmail.com>
2022-04-30 22:18:29 -0400

    Some css and cleaned up table js file.

--- js/table.js
+++ js/table.js
@@ -2,9 +2,12 @@
 
 let table = document.getElementById("main-table");
 
-array.forEach((item, index) => {
-    console.log(index, item.name);
+// Clear placeholder html or old data first.
+table.innerHTML = "";
+
+// Iterate over array, print to html.
+array.forEach((item) => {
     let tr = document.createElement("tr");
     table.appendChild(tr);
-    tr.innerHTML = "<td>" + item.num + "</td><td>" + item.name + "</td><td>" + item.office + "</td><td>" + item.product + "</td><td>" + item.title + "</td><td>" + item.completedDate + "</td>";
+    tr.innerHTML = "<td class='center'>" + item.num + "</td><td>" + item.name + "</td><td>" + item.office + "</td><td>" + item.product + "</td><td>" + item.title + "</td><td class='center'>" + item.completedDate + "</td>";
 });
